664667656c4ad96fcdbc37bd53b1137007205f07
jarek <jarek@192.168.1.106>
2016-12-18 15:21:44 +0100

    dalsze prace

--- index.php
+++ index.php
@@ -8,24 +8,31 @@
     <h1>Ćwiczenia praktyczne z PHP</h1>
     <p>
     <?php
-    //wprowadzenie zmiennyvh
+    //wprowadzenie zmiennych
     $keyword1 = "Ewelina";
-    $keynumber = 7;
+    $keynumber1 = 7;
     $array1 = array("czerwony", "niebieski", "zielony", "fioletowy");
     $array2 = array(1, 5, 7, 8, 13, 17);
     
     //wyświetenie zmiennych
     print "$keyword1</br>";
-    print "$keynumber</br>";
+    print "$keynumber1</br>";
     print "$array1[0]</br>";
     print "$array2[0]</br>";
     
-    //pętla
-    for ($i = 0; $i > $keynumber; $i++) {
-        print "$i</br>";
+    //czy w $array2 znajduje $keynumber
+    for ($i = 0; $i < count($array2); $i++) {
+        if ($array2[$i] == $keynumber1) {
+            print "<p>zmienna jest równa $keynumber1</p>";
+        } 
     }
     
-    ?>
+    //pętla
+    for ($i = 0; $i < $keynumber1; $i++) {
+        print "$i</br>";
+    }
+     
+    ?> 
     </p>
 </body>
 </html>
