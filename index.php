b105a37b04b6849420ed0cc92b296912ee7cbeda
jarek <jarek@192.168.1.106>
2016-12-18 11:05:00 +0100

    dalsze prace

--- index.php
+++ index.php
@@ -8,9 +8,22 @@
     <h1>Ćwiczenia praktyczne z PHP</h1>
     <p>
     <?php
-    $keyword = "Ewelina";
-
-    echo $keyword;
+    //wprowadzenie zmiennyvh
+    $keyword1 = "Ewelina";
+    $keynumber = 7;
+    $array1 = array("czerwony", "niebieski", "zielony", "fioletowy");
+    $array2 = array(1, 5, 7, 8, 13, 17);
+    
+    //wyświetenie zmiennych
+    print "$keyword1</br>";
+    print "$keynumber</br>";
+    print "$array1[0]</br>";
+    print "$array2[0]</br>";
+    
+    //pętla
+    for ($i = 0; $i > $keynumber; $i++) {
+        print "$i</br>";
+    }
     
     ?>
     </p>
