a345ff800cc14d27ee8d54173b5dda3a9f4b5598
jarek <jarek@192.168.1.106>
2016-12-18 08:36:38 +0100

    Pierwszy commmit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Jarosław Dolega</title>
+</head>
+<body>
+    <h1>Ćwiczenia praktyczne z PHP</h1>
+    <p>
+    <?php
+    $keyword = "Ewelina";
+
+    echo $keyword;
+    
+    ?>
+    </p>
+</body>
+</html>
