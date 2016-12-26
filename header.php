9ce2a34cf6f07a837ec33f8b9dd833bba8065839
jarek <jarek@192.168.1.106>
2016-12-26 21:51:32 +0100

    Dodanie plików z językami do nauki

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php
+print 
+'<ul>
+    <li><a href="/index.php">Home</a></li>
+    <li><a href="/html5.php">HTML5</a></li>
+    <li><a href="/css3.php">CSS3</a></li>
+    <li><a href="/php.php">PHP</a></li>
+    <li><a href="/javascript.php">JavaScript</a></li>
+    <li><a href="/sql.php">SQL</a></li>
+    <li><a href="/java.php">JAVA</a></li>
+</ul>'
+?>
